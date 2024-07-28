5b0f0d269d2adb02088d2cc82bff95a10ea60604
cns <skrtn3410@gmail.com>
2024-07-28 10:02:16 +0900

    feat: Splash 화면 추가

--- swifui-playground/coupang/splash.swift
+++ swifui-playground/coupang/splash.swift
@@ -5,4 +5,19 @@
 //  Created by 최낙수 on 7/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        VStack {
+            Image("CoupangLogo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 1000, height: 60)
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
